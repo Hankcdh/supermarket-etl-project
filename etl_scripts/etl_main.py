2ddccd17084da2e7a53d0d0c7a908233c8898b92
Hankcdh <hankcdh@yahoo.com.au>
2024-03-06 13:24:45 +1100

    modified etl init connect bug

--- etl_scripts/etl_main.py
+++ etl_scripts/etl_main.py
@@ -1,8 +1,17 @@
 from etl_subsystems_modules import etl_script
+import time
+
+
+
+
+
+# Use the function before running the ELT process
+
 
 print("Startig etl_main")
 
-etl_script.initialise_connection()
+if not etl_script.initialise_connection():
+    exit(1)
 
 print("Ending etl_main ")
 
--- etl_scripts/etl_subsystems_modules/etl_script.py
+++ etl_scripts/etl_subsystems_modules/etl_script.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import literal_column
 from sqlalchemy.exc import SQLAlchemyError
+import time 
 
 
-def initialise_connection():
+def initialise_connection(max_retries=5, delay_seconds=5):
     print("Starting Initialisation")
     #Define host configuration 
     postsql_host_config = {"DB_NAME" : 'stage_db',
@@ -15,22 +16,27 @@ def initialise_connection():
                             "DB_PORT" : '5432'}
 
     conn_string = f'postgresql+psycopg2://{postsql_host_config["DB_USER"]}:{postsql_host_config["DB_PASS"]}@{postsql_host_config["DB_HOST"]}:{postsql_host_config["DB_PORT"]}/{postsql_host_config["DB_NAME"]}'
+    
+    retries = 0
+    while retries < max_retries:
+        try:
+        # Connection Establishment
+            SQLAlchemy_engine = create_engine(conn_string)
+            connection = SQLAlchemy_engine.connect()
+            print("Connection successful!")
+            connection.close() 
+            return True
+        except SQLAlchemyError as err:
+            print(f"SQLAlchemyError: {err}")
+            retries += 1
+            print(
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
 
-    try:
-    # Connection Establishment
-        SQLAlchemy_engine = create_engine(conn_string)
-        connection = SQLAlchemy_engine.connect()
-        print("Connection successful!")
-        connection.close() 
-        
-
-    except SQLAlchemyError as err:
-        print(f"SQLAlchemyError: {err}")
+        except Exception as err:
+            print(f"Unexpected Error: {err}")
+    print("Max retries reached. Exiting.")
+    return False
 
 
-    except Exception as err:
-        print(f"Unexpected Error: {err}")
-            
-
-
-    print("Ending Initialisation")
+    
